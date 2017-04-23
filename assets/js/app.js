05e429acc8e3ca7599f31cdd7e3a357fe80005e9
shogo-ma <Choroma194@gmail.com>
2017-04-24 01:26:32 +0900

    :+1: add delete button

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,13 +10,20 @@ var getTodos = () => {
     template: `
         <label>
             <input type="checkbox" v-on:click="checkedTodo(todo)"> {{ todo.text }}
+            <button @click="deleteTodo(todo)">delete</button>
         </label>
         `,
     methods: {
       checkedTodo: function(todo) {
         axios.put(`/api/v1/checked/${todo.todo_id}`).then((res) => {
           todo.status = true;
-        });
+        })
+      },
+      deleteTodo: function(todo) {
+        console.log('todo class: delete')
+        axios.delete(`/api/v1/todo/${todo.todo_id}`).then((res) => {
+          this.$emit('delete', todo)
+        })
       }
     }
   })
@@ -27,7 +34,7 @@ var getTodos = () => {
       <ul>
           <div v-for="todo in todos">
               <li v-if="todo.status==todoStatus">
-                  <todo :todo="todo"></todo>
+                  <todo :todo="todo" v-on:delete="deleteTodo"></todo>
               </li>
           </div>
       </ul>
@@ -35,6 +42,12 @@ var getTodos = () => {
     components: {
       todoComponent,
     },
+    methods: {
+      deleteTodo: function(todo) {
+        console.log('todo items class: delete')
+        this.$emit('delete', todo)
+      }
+    }
   })
 
   var todoInputComponent = Vue.component('todo-input-field', {
@@ -55,7 +68,7 @@ var getTodos = () => {
         axios.post("/api/v1/todo", {
           text: this.content
         }).then((res) => {
-            this.$emit('reload');
+          this.$emit('reload');
         })
         this.content = "";
       }
@@ -67,9 +80,9 @@ var getTodos = () => {
       <div>
           <todo-input-field :todos="todos" v-on:reload="reloadTodoList"></todo-input-field>
           <h3>TODO</h3>
-          <todo-items :todo-status=false :todos="todos"></todo-items>
+          <todo-items :todo-status=false :todos="todos" v-on:delete="deleteTodo"></todo-items>
           <h3>DONE</h3>
-          <todo-items :todo-status=true :todos="todos"></todo-items>
+          <todo-items :todo-status=true :todos="todos" v-on:delete="deleteTodo"></todo-items>
       </div>
       `,
     components: {
@@ -88,14 +101,22 @@ var getTodos = () => {
       }
     },
     methods: {
-        reloadTodoList: function() {
-            console.log("reloadTodoList")
-            getTodos().then((res) => {
-                this.todos = res.data;
-            }).catch((res) => {
-                console.log("reloadTodoList error");
-            })
+      reloadTodoList: function() {
+        getTodos().then((res) => {
+          this.todos = res.data;
+        }).catch((res) => {
+          console.log("reloadTodoList error");
+        })
+      },
+      deleteTodo: function(todo) {
+        console.log('todo app class: delete');
+        for (var i = 0; i < this.todos.length; i++) {
+          if (this.todos[i]["todo_id"] == todo.todo_id) {
+            this.todos.splice(i, 1);
+            break;
+          }
         }
+      }
     }
   })
 
